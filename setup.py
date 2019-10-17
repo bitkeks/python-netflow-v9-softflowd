345a5b08ff8ab2ef29fbeb11353ced908735ef92
Carey Metcalfe <carey@cmetcalfe.ca>
2019-10-16 23:25:31 -0400

    Fix setup.py file

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
+from setuptools import setup
 import os
 
-data_files = [(d, [os.path.join(d, f) for f in files])
-              for d, folders, files in os.walk(os.path.join('src', 'config'))]
-
-setup(name='netflow-v9',
+setup(name='netflow',
       version='0.7.0',
-      description='NetFlow v9 parser and collector implemented in Python 3. Developed to be used with softflowd v0.9.9',
+      description='NetFlow v1, v5, and v9 parser and collector implemented in Python 3. Developed to be used with softflowd v0.9.9',
       author='Dominik Pataky',
       author_email='dev@bitkeks.eu',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
+      packages=["netflow"],
       license='MIT'
 )
